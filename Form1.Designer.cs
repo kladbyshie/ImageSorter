944a9715b42df692caefdcbd287d9b8557c32ed8
ngaragulya <ngaragulya@gmail.com>
2020-08-07 15:38:58 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ImageSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.FindTextbox = new System.Windows.Forms.TextBox();
+            this.StoreTextbox = new System.Windows.Forms.TextBox();
+            this.FindButton = new System.Windows.Forms.Button();
+            this.StoreButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FindTextbox
+            // 
+            this.FindTextbox.Location = new System.Drawing.Point(25, 33);
+            this.FindTextbox.Name = "FindTextbox";
+            this.FindTextbox.Size = new System.Drawing.Size(493, 27);
+            this.FindTextbox.TabIndex = 0;
+            // 
+            // StoreTextbox
+            // 
+            this.StoreTextbox.Location = new System.Drawing.Point(25, 86);
+            this.StoreTextbox.Name = "StoreTextbox";
+            this.StoreTextbox.Size = new System.Drawing.Size(493, 27);
+            this.StoreTextbox.TabIndex = 1;
+            // 
+            // FindButton
+            // 
+            this.FindButton.Location = new System.Drawing.Point(544, 31);
+            this.FindButton.Name = "FindButton";
+            this.FindButton.Size = new System.Drawing.Size(135, 29);
+            this.FindButton.TabIndex = 2;
+            this.FindButton.Text = "Select Directory";
+            this.FindButton.UseVisualStyleBackColor = true;
+            this.FindButton.Click += new System.EventHandler(this.FindButton_Click);
+            // 
+            // StoreButton
+            // 
+            this.StoreButton.Location = new System.Drawing.Point(544, 86);
+            this.StoreButton.Name = "StoreButton";
+            this.StoreButton.Size = new System.Drawing.Size(135, 29);
+            this.StoreButton.TabIndex = 3;
+            this.StoreButton.Text = "Select Directory";
+            this.StoreButton.UseVisualStyleBackColor = true;
+            this.StoreButton.Click += new System.EventHandler(this.StoreButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Where to find files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Where to store sorted files";
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(697, 31);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(106, 82);
+            this.SortButton.TabIndex = 6;
+            this.SortButton.Text = "Sort ";
+            this.SortButton.UseVisualStyleBackColor = true;
+            this.SortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 135);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StoreButton);
+            this.Controls.Add(this.FindButton);
+            this.Controls.Add(this.StoreTextbox);
+            this.Controls.Add(this.FindTextbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sorter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox FindTextbox;
+        private System.Windows.Forms.TextBox StoreTextbox;
+        private System.Windows.Forms.Button FindButton;
+        private System.Windows.Forms.Button StoreButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SortButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageSorter
+{
+    public partial class Form1 : Form
+    {
+
+        public static string SearchDirectory { get; set; }
+        public static string StoreDirectory { get; set; }
+        Hashtable DirAndDate = new Hashtable();
+        public static int SortedItemCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            var directory = new FolderBrowserDialog();
+            directory.ShowDialog();
+            SearchDirectory = directory.SelectedPath + @"\";
+            FindTextbox.Text = SearchDirectory;
+        }
+
+        private void StoreButton_Click(object sender, EventArgs e)
+        {
+            var directory = new FolderBrowserDialog();
+            directory.ShowDialog();
+            StoreDirectory = directory.SelectedPath + @"\";
+            StoreTextbox.Text = StoreDirectory;
+        }
+        
+        private void SortButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(SearchDirectory) && !String.IsNullOrEmpty(StoreDirectory))
+            {
+                try
+                {
+                    DirAndDate.Clear();
+                    Thread thread = new Thread(() =>
+                    {
+                        FindFiles();
+                        FolderSetup();
+                        Mover();
+                        MessageBox.Show(String.Format("Sorted {0} / {1} items!", SortedItemCount, DirAndDate.Keys.Count), "Complete!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    });
+                    thread.Start();
+                }
+                catch (Exception b)
+                {
+                    MessageBox.Show("Something went wrong. Writing to log file!", "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    LogWriter(b.Message);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("The two directories seem to be empty. Please navigate to a directory and try again!", "Empty directories", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        void FindFiles()
+        {
+            string[] allfiles = System.IO.Directory.GetFileSystemEntries(SearchDirectory, "*", SearchOption.AllDirectories);
+            foreach (string x in allfiles)
+            {
+                DateTime creation = File.GetLastWriteTime(x);
+                DirAndDate.Add(x, creation.ToString("yyyy-MM-dd"));
+            }
+        }
+
+        public void FolderSetup()
+        {
+            foreach (string x in DirAndDate.Values)
+            {
+                string path = StoreDirectory + x;
+                if (!System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+            }
+        }
+
+        public void Mover()
+        {
+            foreach (string FilePath in DirAndDate.Keys)
+            {
+                try
+                {
+                    FileInfo File = new FileInfo(FilePath);
+                    string EndPath = StoreDirectory + DirAndDate[FilePath].ToString() + @"\" + File.Name;
+                    File.MoveTo(EndPath, true);
+                    SortedItemCount += 1;
+                }
+                catch (Exception e)
+                {
+                    LogWriter("Error: " + e.Message);
+                }
+            }
+        }
+
+        public void LogWriter(string input)
+        {
+
+            using (StreamWriter sw = File.AppendText("log.txt"))
+            {
+                sw.WriteLine(input);
+                sw.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
